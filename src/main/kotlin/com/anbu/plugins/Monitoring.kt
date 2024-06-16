0bca8b9f701e6226af84c18dc093edbe0c12570d
jangkungari <jangkungarim@gmail.com>
2024-06-16 13:50:17 +0700

    initial commit

--- src/main/kotlin/com/anbu/plugins/Monitoring.kt
+++ src/main/kotlin/com/anbu/plugins/Monitoring.kt
@@ -2,10 +2,6 @@ package com.anbu.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import org.slf4j.event.*
 
 fun Application.configureMonitoring() {
     install(CallLogging)
