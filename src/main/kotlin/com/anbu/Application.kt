228fc91efd93f9e3ca1de4fbca85a31ab021f027
jangkungari <jangkungarim@gmail.com>
2024-06-16 13:46:37 +0700

    initial commit

---
+++ src/main/kotlin/com/anbu/Application.kt
@@ -0,0 +1,16 @@
+package com.anbu
+
+import com.anbu.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureKoin()
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+    configureDefaultHeaders()
+}
