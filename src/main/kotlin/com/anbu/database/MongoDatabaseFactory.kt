c5687acfc82d9054402a6ef19a4b6a98bacc94a4
Jangkung <nodameeee@gmail.com>
2025-07-09 13:35:11 +0700

    connection string

--- src/main/kotlin/com/anbu/database/MongoDatabaseFactory.kt
+++ src/main/kotlin/com/anbu/database/MongoDatabaseFactory.kt
@@ -1,26 +1,26 @@
 package com.anbu.database
 
+import com.anbu.data.repository.UserDataSource
+import com.anbu.data.repository.UserDataSourceImpl
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
-import io.github.cdimascio.dotenv.dotenv
 import org.koin.dsl.module
 
-class MongoDatabaseFactory {
-    private val connectionString = System.getenv("MONGO_DB_URI") ?: error("Missing MONGO_DB_URI env")
-    private val client = MongoClient.create(connectionString)
-    val db: MongoDatabase = client.getDatabase("anbuapp")
-}
-
 val mongoModule = module {
-    single<MongoDatabaseFactory> {
-//        val envName = System.getenv("ENV") ?: "dev"
-//        val dotenv = dotenv {
-//            filename = ".env.$envName"
-//            ignoreIfMissing = true
-//        }
-//
-//        val mongoUri = dotenv["MONGO_DB_URI"] ?: error("Missing MONGO_DB_URI")
-        MongoDatabaseFactory()
+
+    single<MongoClient> {
+        val mongoUri = System.getenv("MONGO_DB_URI")
+            ?: error("Missing MONGO_DB_URI env variable")
+        MongoClient.create(mongoUri)
     }
-    single<MongoDatabase> { get<MongoDatabaseFactory>().db }
+
+    single<MongoDatabase> {
+        val client = get<MongoClient>()
+        val dbName = System.getenv("MONGO_DB_NAME") ?: "anbuapp"
+        client.getDatabase(dbName)
+    }
+
+    single<UserDataSource> {
+        UserDataSourceImpl(get())
+    }
 }
--- src/main/kotlin/com/anbu/plugins/Koin.kt
+++ src/main/kotlin/com/anbu/plugins/Koin.kt
@@ -1,9 +1,8 @@
 package com.anbu.plugins
 
-import com.anbu.di.mongoModule
+import com.anbu.database.mongoModule
 import com.anbu.di.repositoryModule
-import io.ktor.server.application.Application
-import io.ktor.server.application.install
+import io.ktor.server.application.*
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
