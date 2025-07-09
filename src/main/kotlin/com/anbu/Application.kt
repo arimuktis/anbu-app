373c0cc55af53135b453d36f212cb5b970229ba2
Jangkung <nodameeee@gmail.com>
2025-07-09 12:36:38 +0700

    fix ssl cacert

--- src/main/kotlin/com/anbu/Application.kt
+++ src/main/kotlin/com/anbu/Application.kt
@@ -1,32 +1,12 @@
 package com.anbu
 
-import com.anbu.database.MongoDatabaseFactory
-import com.anbu.plugins.configureAuth
-import com.anbu.plugins.configureDefaultHeaders
-import com.anbu.plugins.configureKoin
-import com.anbu.plugins.configureMonitoring
-import com.anbu.plugins.configureRouting
-import com.anbu.plugins.configureSerialization
-import com.anbu.plugins.configureSession
-import com.anbu.plugins.configureStatusPages
-import io.github.cdimascio.dotenv.dotenv
+import com.anbu.plugins.*
 import io.ktor.server.application.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
-    val factory = MongoDatabaseFactory()
-
-    val envName = System.getenv("ENV") ?: "dev"
-    val dotenv = dotenv {
-        filename = ".env.$envName"
-        ignoreIfMissing = true
-    }
-
-    val mongoUri = dotenv["MONGO_DB_URI"] ?: error("Missing MONGO_DB_URI")
-
     configureKoin()
     configureSerialization()
     configureMonitoring()
--- src/main/kotlin/com/anbu/database/MongoDatabaseFactory.kt
+++ src/main/kotlin/com/anbu/database/MongoDatabaseFactory.kt
@@ -2,15 +2,24 @@ package com.anbu.database
 
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import io.github.cdimascio.dotenv.dotenv
 import org.koin.dsl.module
 
-class MongoDatabaseFactory {
-    private val connectionString = System.getenv("MONGO_DB_URI")
+class MongoDatabaseFactory(connectionString: String) {
     private val client = MongoClient.create(connectionString)
     val db: MongoDatabase = client.getDatabase("anbuapp")
 }
 
 val mongoModule = module {
-    single { MongoDatabaseFactory() }
+    single<MongoDatabaseFactory> {
+        val envName = System.getenv("ENV") ?: "dev"
+        val dotenv = dotenv {
+            filename = ".env.$envName"
+            ignoreIfMissing = true
+        }
+
+        val mongoUri = dotenv["MONGO_DB_URI"] ?: error("Missing MONGO_DB_URI")
+        MongoDatabaseFactory(mongoUri)
+    }
     single<MongoDatabase> { get<MongoDatabaseFactory>().db }
 }
