381d23d770b12cddaaafa56c463af80b2383be79
Jangkung <nodameeee@gmail.com>
2025-07-09 20:52:12 +0700

    convigure env

--- src/main/kotlin/com/anbu/database/MongoDatabaseFactory.kt
+++ src/main/kotlin/com/anbu/database/MongoDatabaseFactory.kt
@@ -4,19 +4,25 @@ import com.anbu.data.repository.UserDataSource
 import com.anbu.data.repository.UserDataSourceImpl
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import io.github.cdimascio.dotenv.dotenv
 import org.koin.dsl.module
 
 val mongoModule = module {
 
+    val dotenv = dotenv {
+        ignoreIfMalformed = true
+        ignoreIfMissing = true
+    }
+
     single<MongoClient> {
-        val mongoUri = System.getenv("MONGO_DB_URI")
+        val mongoUri = dotenv["MONGO_DB_URI"]
             ?: error("Missing MONGO_DB_URI env variable")
         MongoClient.create(mongoUri)
     }
 
     single<MongoDatabase> {
         val client = get<MongoClient>()
-        val dbName = System.getenv("MONGO_DB_NAME") ?: "anbuapp"
+        val dbName = dotenv["MONGO_DB_NAME"] ?: "anbuapp"
         client.getDatabase(dbName)
     }
 
--- src/main/kotlin/com/anbu/plugins/Session.kt
+++ src/main/kotlin/com/anbu/plugins/Session.kt
@@ -1,6 +1,7 @@
 package com.anbu.plugins
 
 import com.anbu.domain.models.UserSession
+import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.sessions.SessionTransportTransformerEncrypt
@@ -12,13 +13,18 @@ import java.io.File
 
 fun Application.configureSession() {
     install(Sessions) {
-        val secretEncryptKey = hex("00112233445566778899aabbccddeeff") // 16 bytes
-        val secretAuthKey = hex("ffeeddccbbaa99887766554433221100")  // 16 bytes
+        val secretEncryptKey = hex("00112233445566778899aabbccddeeff")
+        val secretAuthKey = hex("ffeeddccbbaa99887766554433221100")
         cookie<UserSession>(
             name = "USER_SESSION",
             storage = directorySessionStorage(File(".session"))
         ) {
-            cookie.secure = true
+            val dotenv = dotenv {
+                ignoreIfMalformed = true
+                ignoreIfMissing = true
+            }
+            val secureFlag = dotenv["FLAG_SECURE"]?.lowercase() == "true"
+            cookie.secure = secureFlag
             transform(SessionTransportTransformerEncrypt(secretEncryptKey, secretAuthKey))
         }
     }
